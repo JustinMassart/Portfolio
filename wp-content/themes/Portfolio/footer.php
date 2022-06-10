08738e15fb85487d7a7a9bebb3d46177bf76d04b
JustinMassart <justin.massart@student.hepl.be>
2022-06-10 17:16:52 +0200

    Updated css

--- wp-content/themes/Portfolio/footer.php
+++ wp-content/themes/Portfolio/footer.php
@@ -1,15 +1,22 @@
 </div>
 <footer class="footer">
 	<h2 class="sro">Pied de page</h2>
-	<div class="footer__socials socials">
-		<ul class="socials__list">
-			<li class="socials__item">Linkedin</li>
-			<li class="socials__item">Github</li>
-		</ul>
-	</div>
-	<div class="footer__others others">
-		<a href="<?= get_permalink ( JMPortfolio_get_template_page ( 'template-conditions' ) ) ?>">Conditions générales</a>
-		<a href="<?= get_permalink ( JMPortfolio_get_template_page ( 'template-legacy' ) ) ?>">Mentions légales</a>
+	<div class="footer__grid">
+		<div class="footer__socials socials">
+			<ul class="socials__list">
+				<li class="socials__item">Linkedin</li>
+				<li class="socials__item">Github</li>
+			</ul>
+		</div>
+		<div class="footer__others others">
+			<a href="<?= get_permalink ( JMPortfolio_get_template_page ( 'template-conditions' ) ) ?>">Conditions générales</a>
+			<a href="<?= get_permalink ( JMPortfolio_get_template_page ( 'template-legacy' ) ) ?>">Mentions légales</a>
+		</div>
+		<div class="footer__logo">
+			<p>
+				GNEGNGENGNEE
+			</p>
+		</div>
 	</div>
 </footer>
 <script type="text/javascript" src="<?= JMPortfolio_mix ( '/js/script.js' ) ?>"></script>
